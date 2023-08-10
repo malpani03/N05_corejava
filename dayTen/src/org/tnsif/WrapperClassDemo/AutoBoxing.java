2dd9efe98afda6b45c37b727678598ff57ca49e8
Rupesh Malpani <Rupesh Malpani@LAPTOP-B01NP7EK>
2023-08-10 11:37:31 +0530

    third commit

---
+++ dayTen/src/org/tnsif/WrapperClassDemo/AutoBoxing.java
@@ -0,0 +1,14 @@
+package org.tnsif.WrapperClassDemo;
+/*Program to demonstrate on AutoBoxing
+ * AutoBoxing is nothing but the conversion of primitive type to object type*/
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		char c='A';
+		//Converting a primitive data type to object type
+		Character ch=c;
+		System.out.println(ch);
+
+	}
+
+}
---
+++ dayTen/src/org/tnsif/WrapperClassDemo/UnBoxing.java
@@ -0,0 +1,14 @@
+package org.tnsif.WrapperClassDemo;
+/*Program to demonstrate on UnBoxing
+ * AutoBoxing is nothing but the conversion of object type to primitive type */
+public class UnBoxing {
+
+	public static void main(String[] args) {
+		Integer num=10;
+		
+		//Converting the object data type to primitive data type
+		int x=num;
+		System.out.println(x);
+	}
+
+}
